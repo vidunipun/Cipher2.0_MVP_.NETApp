aac9a876a59ba2e21207dbbfa6b9634091d23fa6
Vidunipun Rajapaksha <vidunipun69@gmail.com>
2025-11-13 13:28:28 +0530

    first commit

---
+++ Cipher2.0_MVP.Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Cipher2._0_MVP.Server
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
